25bed90f4bfbeede77f3ddd898c850c6c6a000d3
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-06-12 16:45:56 -0300

    fazendo a classe Cliente e suas subclasses

--- src/dados/Individual.java
+++ src/dados/Individual.java
@@ -1,4 +1,22 @@
 package dados;
 
 public class Individual extends Cliente{
+    private String cpf;
+
+    public Individual(int codigo, String nome, String cpf){
+        super(codigo,nome);
+        this.cpf=cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Individual{" +
+                "cpf='" + cpf + '\'' +
+                '}';
+    }
+
 }
